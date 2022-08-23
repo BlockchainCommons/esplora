4aba74ab3a0f2f927af8370c48d547708bcb3fc5
Ehnamuram Enoch <enirox001@gmail.com>
2022-08-23 19:26:08 +0100

    esplora redesign and update

--- client/src/views/footer.js
+++ client/src/views/footer.js
@@ -1,13 +1,34 @@
 import Snabbdom from 'snabbdom-pragma'
 
 const staticRoot = process.env.STATIC_ROOT || ''
-const links = process.env.FOOTER_LINKS ? JSON.parse(process.env.FOOTER_LINKS) : { [staticRoot+'img/github_blue.png']: 'https://github.com/blockstream/esplora' }
+const links = process.env.FOOTER_LINKS ? JSON.parse(process.env.FOOTER_LINKS) : {
+   [staticRoot+'img/github_blue.png']: 'https://github.com/blockstream/esplora',
+   [staticRoot+'img/twitter.png']: 'https://twitter.com/BlockchainComns',
+   [staticRoot+'img/rss_feed.png']: 'https://www.blockchaincommons.com/atom.xml'
+  }
+
+
 
 
 export default ({ t, page }) =>
   <footer className="footer">
     <div className="container">
       <div className="footer_container_content">
+        <div className="footer_container_content_about">
+          <h1 className="footer_container_content_about_heading">
+            About Bitcoin Esplora
+          </h1>
+          <p className="footer_container_content_about_text">
+          Our block explorer allows users to search and view specific data that is published in real-time to the Bitcoin blockchain and the Liquid sidechain. As a Bitcoin explorer, it offers many new features compared to existing block explorer choices - the information it presents is drawn from an up to date Bitcoin Core full node, as well as natively supporting SegWit, bech32, and other modern Bitcoin features. The Blockstream Explorer is intended to be easy to use for the casual user, but will increasingly pack some powerful features under the hood for developers. Read more about <a href="https://medium.com/shapeshift-stories/whats-the-deal-with-block-explorers-9fcdb161e788">Block Explorers</a>
+          </p>
+          <div className="footer_container_content_about_bc">
+              <img className="main-nav-container-image" alt="BC Commons Logo" src='img/bc_logo_light.png'/>
+              <p>
+                This site is a proof-of-concept. It is intended to demonstrate the <a href="https://github.com/BlockchainCommons/Gordian#gordian-principles">Principles</a> of the <a href="https://github.com/BlockchainCommons/Gordian/blob/master/README.md">Gordian Architecture</a>. Use at your own risk. For more info on this site, please see its repo. For more info on Blockchain Commons, please see our <a href="https://www.blockchaincommons.com/">main web page</a>. If you'd like to become a sponsor of work like this, please visit our <a href="https://github.com/sponsors/BlockchainCommons">Sponsors Page</a>.
+              </p>
+            
+          </div>
+        </div>
         <div className="language">
           <form method="get">
             { !process.browser && Object.entries(page.query).map(([k, v]) =>
@@ -30,7 +51,6 @@ export default ({ t, page }) =>
                 </a>
             ) }
           </div>
-
           { (process.env.ONION_V3) &&
             <div className="footer_container_content_row_onion_container">
               <div className="footer_container_content_row_onion_icon"></div>
@@ -43,7 +63,7 @@ export default ({ t, page }) =>
         </div>
         <div className="footer_container_content_copyright">
           <div>
-          { process.env.TERMS && <span><a href={ process.env.TERMS } target="_blank">Terms &amp; </a></span> }
+          { process.env.TERMS && <span><a href={ process.env.TERMS } target="_blank">Terms &amp; </a></span>}
           { process.env.PRIVACY && <span><a href={ process.env.PRIVACY } target="_blank">Privacy</a></span> }
           </div>
           <div>{ process.env.SITE_FOOTER || t`Powered by esplora` }</div>
--- client/src/views/navbar-menu.js
+++ client/src/views/navbar-menu.js
@@ -1,7 +1,6 @@
 import Snabbdom from 'snabbdom-pragma'
 import navToggle from './nav-toggle'
 
-
 const items  = process.env.MENU_ITEMS && JSON.parse(process.env.MENU_ITEMS)
     , active = process.env.MENU_ACTIVE
 
@@ -10,6 +9,10 @@ const staticRoot = process.env.STATIC_ROOT || ''
 export default ({ t, theme, page }) =>
 
 <div className="main-nav-container">
+  <span>
+      <img className="main-nav-container-image" alt="BC Commons Logo" src='img/bc_logo_light.png'/>
+  </span>
+  <h4 className="main-nav-container-name">Blockchain Commons</h4>
   <ul className="main-nav">
     { items && Object.entries(items).map(([ name, url ]) =>
         <li id={name.replace(/ /g, '')} className={`nav-item ${name == active ? 'active' : ''}`}>
